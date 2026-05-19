de091d3a6bcea5fbc6950f161ab9c8c04c66b65e
Kenneth <lordlqft0@gmail.com>
2026-05-19 13:55:22 -0300

    Feat: Atividade 5_1
    Atividade 4 usando While

---
+++ Atividade5_1.cs
@@ -0,0 +1,13 @@
+/* Usando while:
+Console.WriteLine("Digite um número para ver a tabuada: ");
+int numero = int.Parse(Console.ReadLine()!);
+
+int i = 1;
+while (i <= 10)
+{
+    Console.Clear();
+    int resultado = numero * i;
+    Console.WriteLine($"{numero} x {i} = {resultado}");
+    i++;
+}
+*/
