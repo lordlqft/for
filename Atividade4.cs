73871e0805b7c2dad32dddee5898acb1da6672c0
Kenneth <lordlqft0@gmail.com>
2026-05-19 13:53:34 -0300

    Feat: Atividade4
    Cálculo de tabuada através de número obtido pelo usuário, utilizando for

---
+++ Atividade4.cs
@@ -0,0 +1,11 @@
+/*
+Console.WriteLine("Digite um número para ver a tabuada: ");
+int numero = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.Clear();
+    int resultado = numero * i;
+    Console.WriteLine($"{numero} x {i} = {resultado}");
+}
+*/
